1492f60d1fdba59660373683c1b1170f7498b725
karnandohuang <karnandohuang@outlook.com>
2026-03-23 20:24:54 +0700

    add styling to input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,13 @@
 export default function Input ({ textarea, label, ...props }) {
+    const classes = 'w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600'
     return (
-        <p>
-            <label>{label}</label>
-            {textarea ? <textarea {...props} /> : <input {...props} /> }
+        <p className="flex flex-col gap-1 my-4">
+            <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
+            {
+                textarea ?
+                    <textarea {...props} className={classes} />
+                    : <input {...props} className={classes} />
+            }
         </p>
     )
 }
