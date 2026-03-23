fa883072b50708be4e8f963b8429ee288dc5cc0d
karnandohuang <karnandohuang@outlook.com>
2026-03-23 21:28:01 +0700

    added NoProjectSelected.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import {Sidebar} from "./components/Sidebar.jsx";
 import {NewProject} from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import {useState} from "react";
 
 function App() {
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectId: undefined,
+        projects: []
+    })
+
+    function handleStartProject() {
+        setProjectsState(prevState => ({
+            ...prevState,
+            selectedProjectId: null
+        }))
+    }
+
+    let content
+
+    if (projectsState.selectedProjectId === null) {
+        content = <NewProject />
+    } else if (projectsState.selectedProjectId === undefined) {
+        content = <NoProjectSelected onStartAddProject={handleStartProject} />
+    }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-        <Sidebar />
-        <NewProject />
+        <Sidebar onStartAddProject={handleStartProject} />
+        {content}
     </main>
   );
 }
