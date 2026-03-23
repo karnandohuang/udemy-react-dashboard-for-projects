59acdce45028b108802b40e2885d4451945954f8
karnandohuang <karnandohuang@outlook.com>
2026-03-23 20:11:32 +0700

    added skeleton for NewProject and Input

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,19 @@
+import Input from "./Input.jsx";
+
+export function NewProject() {
+    return (
+        <>
+            <div>
+                <menu>
+                    <li><button>Cancel</button></li>
+                    <li><button>Save</button></li>
+                </menu>
+                <div>
+                    <Input label={'Title'} />
+                    <Input label={'Description'} textarea />
+                    <Input label={'Due date'} />
+                </div>
+            </div>
+        </>
+    )
+}
